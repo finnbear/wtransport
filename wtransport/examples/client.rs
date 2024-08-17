c12bc9b9bd9175a125b1c4e2cbfe42240c4bcca7
Finn Bear <finnbearlabs@gmail.com>
2024-08-16 21:16:41 -0700

    Fix example.

--- wtransport/examples/client.rs
+++ wtransport/examples/client.rs
@@ -16,5 +16,5 @@ async fn main() {
 
     let mut stream = connection.open_bi().await.unwrap().await.unwrap();
     stream.0.write_all(b"HELLO").await.unwrap();
-    stream.0.finish().await.unwrap();
+    stream.0.finish().unwrap();
 }
